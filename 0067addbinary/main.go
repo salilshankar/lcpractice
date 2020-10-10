0837021d0a4716950bb7ed83348cd9a9f6a94370
Salil Shankar <mno239@gmail.com>
2020-10-10 19:23:52 +0530

    linting stuff

--- 0067addbinary/main.go
+++ 0067addbinary/main.go
@@ -9,112 +9,109 @@ func main() {
 }
 
 func addBinary(a string, b string) string {
-    
+
 	if a == "0" && a == b {
-			return "0"
+		return "0"
 	}
-	
+
 	if a == "1" && a == b {
-			return "10"
-	} 
-	
-	if len(a) == len(b) && len(a) == 1 {
-			return "1"
+		return "10"
 	}
-	
+
+	if len(a) == len(b) && len(a) == 1 {
+		return "1"
+	}
+
 	lenA := len(a)
-lenB := len(b)
+	lenB := len(b)
 
-ans := []byte{}
+	ans := []byte{}
 
-if lenA > lenB {
+	if lenA > lenB {
 		ans = addBinaryExt(a, b)
-} else {
+	} else {
 		ans = addBinaryExt(b, a)
-}
+	}
 
-return string(ans)
+	return string(ans)
 }
 
 func addBinaryExt(larger, smaller string) []byte {
 	bytes := "01"
 	zero := bytes[0]
-	
+
 	// "Make another string called smalleMatched, filled with zeros, whose length matches to the larger string.  And then, copy the contents of smaller one from the bottom. For example, larger string 1010 and smaller string 11. So the smallerMatched contains 0011."
-smallerMatched := make([]byte, len(larger))
-for i := range smaller {
-		smallerMatched[len(smallerMatched) - 1 - i] = smaller[len(smaller) - 1 - i]
-}
-	
+	smallerMatched := make([]byte, len(larger))
+	for i := range smaller {
+		smallerMatched[len(smallerMatched)-1-i] = smaller[len(smaller)-1-i]
+	}
+
 	for i := range smallerMatched {
-			if i < len(smallerMatched) - len(smaller) {
-				smallerMatched[i] = zero
+		if i < len(smallerMatched)-len(smaller) {
+			smallerMatched[i] = zero
 		}
 	}
-	
-	
-//do a bitwise add factoring in carry
-ans := make([]byte, len(larger) + 1)
-ans[0] = zero
-var carry byte
-carry = zero
-for i := range larger {
+
+	//do a bitwise add factoring in carry
+	ans := make([]byte, len(larger)+1)
+	ans[0] = zero
+	var carry byte
+	carry = zero
+	for i := range larger {
 		sum := add(larger[len(larger)-1-i], smallerMatched[len(smallerMatched)-1-i], carry)
 		carry = sum[0]
-		ans[len(ans) - 1 - i] = sum[1]
-}
+		ans[len(ans)-1-i] = sum[1]
+	}
 
-
-ans[0] = carry
+	ans[0] = carry
 
 	if ans[0] == zero {
-			ans = ans[1:]
+		ans = ans[1:]
 	}
-	
-return ans
+
+	return ans
 }
 
 func add(a, b, carry byte) []byte {
-	
+
 	bytes := "01"
 	zero := bytes[0]
 	one := bytes[1]
-	
-if a == b && a == zero {
-					// "0 + 0 + carry"
+
+	if a == b && a == zero {
+		// "0 + 0 + carry"
 		return addCarry([]byte{zero, zero}, carry)
-} else if a == b && a == one {
-					// "1 + 1 + carry"
+	} else if a == b && a == one {
+		// "1 + 1 + carry"
 		return addCarry([]byte{one, zero}, carry)
-} 
+	}
 
 	// "1 + 0 + carry or 0 + 1 + carry"
-return addCarry([]byte{zero, one}, carry)
+	return addCarry([]byte{zero, one}, carry)
 }
 
 func addCarry(a []byte, carry byte) []byte {
- 
+
 	bytes := "01"
 	zero := bytes[0]
 	one := bytes[1]
-	
+
 	// "carry 0 has no consequence"
-if carry == zero {
-	return a
-}
-	
+	if carry == zero {
+		return a
+	}
+
 	// "00 + 1"
-if a[0] == a[1] && a[0] == zero {
-	return []byte{zero, one}
-}
-	
+	if a[0] == a[1] && a[0] == zero {
+		return []byte{zero, one}
+	}
+
 	//  "10 + 1"
-if a[1] == zero {
-	return []byte{one, one}
-}
+	if a[1] == zero {
+		return []byte{one, one}
+	}
 
 	// "01 + 1"
-return []byte{one, zero}
+	return []byte{one, zero}
 
 }
-
